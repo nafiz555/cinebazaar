8bb58ad21ef39467e60ea9d3201d0014ff8629cc
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-21 19:38:02 +0600

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'soulofdrama') # Support Chat Link Wi
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
