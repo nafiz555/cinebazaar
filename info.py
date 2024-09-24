557fa9a0169097a058d2eb8c47438bce0730612a
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-24 20:22:19 +0600

    Update info.py

--- info.py
+++ info.py
@@ -71,10 +71,10 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'shsjjshch') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CineBaazar')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CineBazaar')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'soulofdrama') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CineDramaGroup')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CineDrama')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Netflix_Dramas')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'CineDramaGroup') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
