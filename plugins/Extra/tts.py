f86b2a98fc6d72b1497b9a1a7e0c80a521616a8d
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-25 10:06:17 +0600

    Update tts.py

--- plugins/Extra/tts.py
+++ plugins/Extra/tts.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @CineDrama
+# Subscribe YouTube Channel For Amazing Bot @@CineDrama
+# Ask Doubt on telegram @@CineDrama
 
 
 import traceback
