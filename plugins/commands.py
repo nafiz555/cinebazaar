a915eaeb8473eadf1d2a52fe438351a39b5ea223
Nafis777777 <115471692+Nafis777777@users.noreply.github.com>
2024-09-25 13:03:35 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -562,7 +562,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = '@VJ_Botz  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@CineDrama  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
