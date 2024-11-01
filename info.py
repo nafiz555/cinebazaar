7682e88dce2f1c4ac037d1afffb1b160db6f81dc
nafiz555 <164049630+nafiz555@users.noreply.github.com>
2024-11-01 17:40:36 +0600

    Update info.py

--- info.py
+++ info.py
@@ -46,9 +46,9 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nafiz:nafiz@cluster0.bfu9sqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://video:video@video.jn0qm.mongodb.net/?retryWrites=true&w=majority&appName=Video")
+DATABASE_NAME = environ.get('DATABASE_NAME', "video")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'video')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
