6d765d7c6243a2ddd3a7ffe9fc8f469a59fef4f1
nafiz555 <164049630+nafiz555@users.noreply.github.com>
2024-11-10 14:14:53 +0600

    Update info.py

--- info.py
+++ info.py
@@ -46,8 +46,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://video:video@video.jn0qm.mongodb.net/?retryWrites=true&w=majority&appName=Video")
-DATABASE_NAME = environ.get('DATABASE_NAME', "video")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://video:Video@video.jn0qm.mongodb.net/?retryWrites=true&w=majority&appName=Video")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Video")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Video')
 
 # Premium And Referal Settings
